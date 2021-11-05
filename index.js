93ae4647d694113ad7a24340a34623463d829189
+ <karis@DESKTOP-IE7QI1F.localdomain>
2021-11-05 15:49:54 -0500

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somevalue) {
+  let result
+  if (somevalue < 401) {
+    result = 'This one is on me!'
+  } else if (somevalue > 2000 && somevalue <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (somevalue > 2500) {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(somecity) {
+  let msg = (somecity === 'NYC') ? "Ok, sounds good." : "No go."
+  return msg
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(somegenerosity) {
+  switch (somegenerosity) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
